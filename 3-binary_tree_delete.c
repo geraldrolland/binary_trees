466710c6a049451e2c3fe8087465fffe692052ca
geraldrolland <geraldrolland123@gmail.com>
2024-02-05 14:44:18 -0800

    binary tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -5,43 +5,20 @@
  * @tree: pointer to the root node of the binary tree
  * Return: void
  */
-
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree != NULL)
 	{
-		if (tree->parent == NULL && (tree->left == NULL && tree->right == NULL))
-			free(tree);
 		if (tree->left != NULL)
 		{
 			binary_tree_delete(tree->left);
-			free(tree->left);
-			tree->left = NULL;
-			if (tree->right != NULL)
-			{
-				binary_tree_delete(tree->right);
-				free(tree->right);
-				tree->right = NULL;
-				return;
-
-			}
-			return;
 		}
 		if (tree->right != NULL)
 		{
 			binary_tree_delete(tree->right);
-			free(tree->right);
-			tree->right = NULL;
-			if (tree->left != NULL)
-			{
-				binary_tree_delete(tree->left);
-				free(tree->left);
-				tree->left = NULL;
-				return;
-			}
-			return;
 		}
-		tree->parent = NULL;
-		return;
+
+		free(tree);
 	}
 }
+
